b1b626db28e4f127e7ba03ae356f20c7682418a5
Aryan <aryanbijaioistg6@gmail.com>
2026-09-23 00:30:37 +0530

    testbench for vga_timing_gen added

--- graphics_accelerator.srcs/sim_1/new/vga_timing_test.v
+++ graphics_accelerator.srcs/sim_1/new/vga_timing_test.v
@@ -21,5 +21,32 @@
 
 
 module vga_timing_test;
+reg clk;
+reg reset;
 
+wire h_sync,v_sync,vid_active;
+wire [9:0]pixel_x,pixel_y;
+
+vga_timing_gen(.clk(clk),.reset(reset),.pixel_x(pixel_x),.pixel_y(pixel_y),.vid_active(vid_active),
+                .hsync(hsync),.vsync(vsync));
+
+always begin
+    #20 clk = ~clk;
+end
+
+initial begin
+    clk <= 1'b0;
+    reset <= 1'b1;
+    #100;         //hold reset to 1 for 100 ns, so that we make sure it works on the correct clock pulse
+    reset <= 1'b0;
+    //for 1 frame, run for 20ms, or 2*10^6 ns, since display is 640x480@50, so theres 50 frames per second, which is 1 frame every 20ms 
+    #20000000;
+    
+    $display("Simulation completed");
+    $finish;
+end
+
+initial begin  //the 0 after % removes any trailing 0s)
+    $monitor("Time=%0t ns | reset = %b | h_x = %0d | v_y = %0d | vid_active = %b | hsync = %b | vsync = %b",$time,reset,pixel_x,pixel_y,vid_active,hsync,vsync);
+    end
 endmodule
